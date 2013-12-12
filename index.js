c810e79a7b0d9f0feab99905a642d1788281fb44
Ryan Arana <ryan.arana@gmail.com>
2013-12-12 13:36:29 -0800

    Make things actually work

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-require('./server').start({
-  name: 'blog.ryanarana.com',
-  publicPath: 'build'
+var wintersmith = require('wintersmith');
+var env = wintersmith('./config.json');
+
+env.build(function(error) {
+  if (error) throw error;
+  require('./server').start({
+    name: 'blog.ryanarana.com',
+    publicPath: 'build'
+  });
 });
